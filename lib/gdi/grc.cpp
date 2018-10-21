2f1f16de02856c83e543a9961e2b859c4176936d
openesi <openesi.eu@gmail.com>
2018-10-21 17:34:51 +0200

    Fix dm800se

--- lib/gdi/grc.cpp
+++ lib/gdi/grc.cpp
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <fstream>
 #include <lib/gdi/grc.h>
 #include <lib/gdi/font.h>
 #include <lib/base/init.h>
@@ -43,22 +42,6 @@ gRC::gRC(): rp(0), wp(0)
 		eDebug("[gRC] thread created successfully");
 #endif
 }
-
-#ifdef CONFIG_ION
-void gRC::lock()
-{
-#ifndef SYNC_PAINT
-	pthread_mutex_lock(&mutex);
-#endif
-}
-
-void gRC::unlock()
-{
-#ifndef SYNC_PAINT
-	pthread_mutex_unlock(&mutex);
-#endif
-}
-#endif
 
 DEFINE_REF(gRC);
 
@@ -207,11 +190,7 @@ void *gRC::thread()
 				if (!idle)
 				{
 					if (!m_spinner_enabled)
-					{
 						eDebug("[gRC] main thread is non-idle! display spinner!");
-							std::ofstream dummy("/tmp/doPythonStackTrace");
-							dummy.close();
-					}
 					enableSpinner();
 				} else
 					disableSpinner();
