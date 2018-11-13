9c1d4b82f78c299bac0d9a7787526e0b05bdc473
openesi <openesi.eu@gmail.com>
2018-11-13 19:36:41 +0100

    fix ngettext

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -7,6 +7,7 @@ from boxbranding import getBoxType, getMachineBuild, getBrandOEM
 from SystemInfo import SystemInfo
 import os
 from time import sleep
+from gettext import ngettext
 
 config.av = ConfigSubsection()
 if getBrandOEM() in ('azbox'):
