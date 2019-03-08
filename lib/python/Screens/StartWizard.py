a3e2f3e0d9142438b6ad6e3e9520bc7bd2288663
OpenESI <openesi.eu@gmail.com>
2019-03-08 21:48:31 +0100

    Change skin for overscan Adrenalinnrw_Mod_RED
    Change skin for overscan Adrenalinnrw_Mod_RED

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -18,7 +18,7 @@ from LanguageSelection import LanguageWizard
 config.misc.firstrun = ConfigBoolean(default = True)
 config.misc.languageselected = ConfigBoolean(default = True)
 config.misc.videowizardenabled = ConfigBoolean(default = True)
-config.misc.do_overscanwizard = ConfigBoolean(default = OverscanWizard and config.skin.primary_skin.value == "AeonFHD_MOD/skin.xml")
+config.misc.do_overscanwizard = ConfigBoolean(default = OverscanWizard and config.skin.primary_skin.value == "Adrenalinnrw_Mod_RED/skin.xml")
 
 class StartWizard(WizardLanguage, Rc):
 	def __init__(self, session, silent = True, showSteps = False, neededTag = None):
